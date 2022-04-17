7eafb7642c5b939f1a2f7b220973beff9f224271
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-04-17 23:04:05 +0700

    Updatee

--- Yone/__help__.py
+++ Yone/__help__.py
@@ -10,8 +10,7 @@ from telegram.ext.dispatcher import DispatcherHandlerStop
 from os.path import isfile
 
 
-HELP_STRINGS = """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with. """.format(
+HELP_STRINGS = """Here is the help for the {} module :. """.format(
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
